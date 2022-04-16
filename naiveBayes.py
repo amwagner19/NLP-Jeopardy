8c95deef1eff396abc7692c5ea3de5279d93a5b1
Tom <tscott1999@sbcglobal.net>
2022-04-16 10:15:08 -0500

    Made naiveBayes perform grid search for parameters

--- naiveBayes.py
+++ naiveBayes.py
@@ -2,10 +2,20 @@ from preprocessing import preprocessData
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
 import numpy as np
 
 # Read in the data
-x_train, x_test, x_dev, y_train, y_test, y_dev = preprocessData()
+x_train, x_test, y_train, y_test = preprocessData()
+
+# Define parameters for grid search
+parameters = {
+    'vect__ngram_range': [(1, 1), (1, 2), (2, 2)],
+    'tfidf__norm': ('l1', 'l2'),
+    'tfidf__use_idf': (True, False),
+    'clf__fit_prior': (True, False),
+    'clf__alpha': (1, 1e-1, 1e-2, 1e-3)
+}
 
 # Create a pipeline for NB using CountVectorizer, TfidfTransformer, and MultinomialNB
 text_clf = Pipeline([
@@ -13,9 +23,13 @@ text_clf = Pipeline([
     ('tfidf', TfidfTransformer()),
     ('clf', MultinomialNB())])
 
-# Fit the pipeline on the training data
-text_clf.fit(x_train, y_train)
+# Fit the pipeline using grid search on the training data
+gs_clf = GridSearchCV(text_clf, parameters, cv=5, n_jobs=-1)
+gs_clf = gs_clf.fit(x_train, y_train)
+
+# Determine best estimator
+print(gs_clf.best_estimator_)
 
 # Predict on test data and get accuracy
-predicted = text_clf.predict(x_test)
+predicted = gs_clf.best_estimator_.predict(x_test)
 print(np.mean(predicted == y_test))

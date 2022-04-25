672b829509714f8413e0cd8d2319e994d002b686
Anne Wagner <amwagner19@gmail.com>
2022-04-24 21:59:23 -0500

    Added Logistic regression for count vector

---
+++ rnn.py
@@ -0,0 +1,5 @@
+import numpy as np
+from preprocessing import preprocessData
+from keras.preprocessing.text import Tokenizer
+from keras import models, layers
+from sklearn.model_selection import train_test_split

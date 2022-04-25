5bbbfc095a7d3f48f03fb1039966558e501248e8
Tom <tscott1999@sbcglobal.net>
2022-04-25 13:31:38 -0500

    Comments for CNN and RNN

--- cnn.py
+++ cnn.py
@@ -8,6 +8,7 @@ from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = preprocessData()
 x_train, x_dev, y_train, y_dev = train_test_split(x_train, y_train, test_size=0.1, random_state=329)
 
+# Tokenize training and dev data
 tokenizer = Tokenizer(num_words=2000)
 tokenizer.fit_on_texts(x_train)
 sequences = tokenizer.texts_to_sequences(x_train)
@@ -16,10 +17,9 @@ y_train = np.asarray(y_train).astype('float32')
 x_dev = tokenizer.texts_to_matrix(x_dev, mode='binary')
 y_dev = np.asarray(y_dev).astype('float32')
 
+# Determine the layers of the model
 model = models.Sequential()
-
 model.add(layers.Embedding(input_dim=2000, output_dim=128))
-
 model.add(layers.Conv1D(filters=256, kernel_size=3, activation='relu'))
 model.add(layers.GlobalMaxPooling1D())
 model.add(layers.Dense(512))
@@ -28,11 +28,13 @@ model.add(layers.Dropout(0.25))
 model.add(layers.Dense(2))
 model.add(layers.Activation('softmax'))
 
+# Compile model with accuracy metric and fit on the training/validation data
 model.compile(loss='sparse_categorical_crossentropy',
               optimizer='rmsprop',
               metrics=['accuracy'])
 model.fit(x_train, y_train, epochs=10, batch_size=16, verbose=1, validation_data=(x_dev, y_dev))
 
+# Tokenize test data and evaluate the model
 x_test = tokenizer.texts_to_matrix(x_test, mode='binary')
 y_test = np.asarray(y_test).astype('float32')
 results = model.evaluate(x_test, y_test)
--- rnn.py
+++ rnn.py
@@ -8,6 +8,7 @@ from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = preprocessData()
 x_train, x_dev, y_train, y_dev = train_test_split(x_train, y_train, test_size=0.1, random_state=329)
 
+# Tokenize training and dev data
 tokenizer = Tokenizer(num_words=2000)
 tokenizer.fit_on_texts(x_train)
 sequences = tokenizer.texts_to_sequences(x_train)
@@ -16,18 +17,19 @@ y_train = np.asarray(y_train).astype('float32')
 x_dev = tokenizer.texts_to_matrix(x_dev, mode='binary')
 y_dev = np.asarray(y_dev).astype('float32')
 
+# Determine the layers of the model
 model = models.Sequential()
-
 model.add(layers.Embedding(input_dim=2000, output_dim=32))
-
 model.add(layers.SimpleRNN(32))
 model.add(layers.Dense(1, activation='sigmoid'))
 
+# Compile model with accuracy metric and fit on the training/validation data
 model.compile(loss='binary_crossentropy',
               optimizer='rmsprop',
               metrics=['accuracy'])
 model.fit(x_train, y_train, epochs=10, batch_size=16, verbose=1, validation_data=(x_dev, y_dev))
 
+# Tokenize test data and evaluate the model
 x_test = tokenizer.texts_to_matrix(x_test, mode='binary')
 y_test = np.asarray(y_test).astype('float32')
 results = model.evaluate(x_test, y_test)

b30251b2d8a9ead1162d7911590ea96d7e351503
Anne Wagner <amwagner19@gmail.com>
2022-05-02 23:54:46 -0500

    Still adding to sparse vector, dense vector complete, should be done tuesday

--- logisticRegression.py
+++ logisticRegression.py
@@ -9,6 +9,9 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 import gensim
 import gensim.downloader as api
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.model_selection import GridSearchCV
+
 
 # Read in the data
 x_train, x_test, y_train, y_test = preprocessData()
@@ -17,20 +20,24 @@ x_train, x_dev, y_train, y_dev = train_test_split(x_train, y_train, test_size=0.
 tokenizer = Tokenizer(num_words=2000)
 tokenizer.fit_on_texts(x_train)
 
+
+
 def sparseVector(): 
     #Sparse Vector - Count vectorizer
-    
-    x_sparse_train = tokenizer.texts_to_matrix(x_train, mode='count')
+    vectorizer = CountVectorizer()
+    x_sparse_train = vectorizer.fit_transform(x_train)
     y_sparse_train = np.asarray(y_train).astype('float32')
-    x_sparse_dev = tokenizer.texts_to_matrix(x_dev, mode='count')
+    x_sparse_dev = vectorizer.fit_transform(x_dev)
     y_sparse_dev = np.asarray(y_dev).astype('float32')
-    x_sparse_test = tokenizer.texts_to_matrix(x_test, mode='count')
+    x_sparse_test = vectorizer.fit_transform(x_test)
     y_sparse_test = np.asarray(y_test).astype('float32')
     
     model=LogisticRegression(multi_class="auto",random_state=329).fit(x_sparse_train,y_sparse_train)
     
     dev_pred = model.predict(x_sparse_dev)
     dev_results=accuracy_score(y_sparse_dev,dev_pred)
+    
+    
     
     test_pred = model.predict(x_sparse_test)
     test_results= accuracy_score(y_sparse_test, test_pred)
@@ -40,7 +47,6 @@ def sparseVector():
     #Dev Accuracy: 0.5557899671823723
     #Test Accuracy: 0.5618612283169245
     
-
 def denseVector(): 
     #Dense Vector - word2vec
     x_dense_train = [word_tokenize(i) for i in x_train]
@@ -57,6 +63,7 @@ def denseVector():
     X_dev_vect=np.array([np.array([model.wv[i] for i in ls if i in words])for ls in x_dense_dev])
     X_test_vect = np.array([np.array([model.wv[i] for i in ls if i in words])for ls in x_dense_test])
     
+
     X_train_vect_avg = []
     for v in X_train_vect:
         if v.size:
@@ -77,20 +84,24 @@ def denseVector():
             X_test_vect_avg.append(v.mean(axis=0))
         else:
             X_test_vect_avg.append(np.zeros(100, dtype=float))
-
     
-            
     logModel=LogisticRegression(multi_class="auto",random_state=329).fit(X_train_vect_avg,y_dense_train)
     
+    # print(len(y_dense_dev))
     dev_pred = logModel.predict(X_dev_vect_avg)
-    dev_results=accuracy_score(y_dense_test,dev_pred)
+    dev_pred=np.asarray(dev_pred).astype('float32')
+    dev_results=accuracy_score(y_dense_dev,dev_pred)
+    
+
     
     test_pred = logModel.predict(X_test_vect_avg)
     test_results= accuracy_score(y_dense_test, test_pred)
     
     print(dev_results)
     print(test_results)
-    
+    #Dev Accuracy: 0.5590131270511017
+    #Test Accuracy:0.5547116736990154
+
            
     
-denseVector()
+sparseVector()
